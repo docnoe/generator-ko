306e85e8c7f76e65dd7e48d27d972d0508f91bac
Johannes Noe <noe@ferrari-electronic.de>
2015-01-24 15:50:44 +0100

    more coffeescript support

--- component/index.js
+++ component/index.js
@@ -6,48 +6,62 @@ var chalk = require('chalk');
 
 var ComponentGenerator = yeoman.generators.NamedBase.extend({
 
-  detectCodeLanguage: function() {
-    this.usesTypeScript = fs.existsSync('src/app/startup.ts');
-    this.codeFileExtension = this.usesTypeScript ? '.ts' : '.js';
-  },
-
-  init: function () {
-    var codeLanguage = this.usesTypeScript ? 'TypeScript' : 'JavaScript';
-    console.log('Creating component \'' + this.name + '\' (' + codeLanguage + ')...');
-    this.componentName = this.name;
-    this.dirname = 'src/components/' + this._.dasherize(this.name) + '/';
-    this.filename = this._.dasherize(this.name);
-    this.viewModelClassName = this._.classify(this.name);
-  },
-
-  template: function () {
-    var codeExtension = this.usesTypeScript ? '.ts' : '.js';
-    this.copy('view.html', this.dirname + this.filename + '.html');
-    this.copy('viewmodel' + this.codeFileExtension, this.dirname + this.filename + this.codeFileExtension);
-  },
-
-  addComponentRegistration: function() {
-    var startupFile = 'src/app/startup' + this.codeFileExtension;
-    readIfFileExists.call(this, startupFile, function(existingContents) {
-        var existingRegistrationRegex = new RegExp('\\bko\\.components\\.register\\(\s*[\'"]' + this.filename + '[\'"]');
-        if (existingRegistrationRegex.exec(existingContents)) {
-            this.log(chalk.white(this.filename) + chalk.cyan(' is already registered in ') + chalk.white(startupFile));
-            return;
+    detectCodeLanguage: function() {
+        this.usesTypeScript = fs.existsSync('src/app/startup.ts');
+        this.usesCoffeeScript = fs.existsSync('src/app/startup.coffee');
+        this.codeFileExtension = ".js";
+        if (this.usesTypeScript) {
+            this.codeFileExtension = ".ts";
         }
-
-        var token = '// [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]',
-            regex = new RegExp('^(\\s*)(' + token.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') + ')', 'm'),
-            modulePath = 'components/' + this.filename + '/' + this.filename,
-            lineToAdd = 'ko.components.register(\'' + this.filename + '\', { require: \'' + modulePath + '\' });',
-            newContents = existingContents.replace(regex, '$1' + lineToAdd + '\n$&');
-        fs.writeFile(startupFile, newContents);
-        this.log(chalk.green('   registered ') + chalk.white(this.filename) + chalk.green(' in ') + chalk.white(startupFile));
-
-        if (fs.existsSync('gulpfile.js')) {
-            this.log(chalk.magenta('To include in build output, reference ') + chalk.white('\'' + modulePath + '\'') + chalk.magenta(' in ') + chalk.white('gulpfile.js'));
+        if (this.usesCoffeeScript) {
+            this.codeFileExtension = ".coffee";
         }
-    });
-  }
+    },
+
+    init: function() {
+        var codeLanguage = "JavaScript" //this.usesTypeScript ? 'TypeScript' : 'JavaScript';
+        if (this.usesTypeScript) {
+            codeLanguage = "TypeScript";
+        }
+        if (this.usesCoffeeScript) {
+            codeLanguage = "CoffeeScript";
+        }
+        console.log('Creating component \'' + this.name + '\' (' + codeLanguage + ')...');
+        this.componentName = this.name;
+        this.dirname = 'src/components/' + this._.dasherize(this.name) + '/';
+        this.filename = this._.dasherize(this.name);
+        this.viewModelClassName = this._.classify(this.name);
+    },
+
+    template: function() {
+        this.copy('view.html', this.dirname + this.filename + '.html');
+        this.copy('viewmodel' + this.codeFileExtension, this.dirname + this.filename + this.codeFileExtension);
+    },
+
+    addComponentRegistration: function() {
+        var startupFile = 'src/app/startup' + this.codeFileExtension;
+        readIfFileExists.call(this, startupFile, function(existingContents) {
+            var existingRegistrationRegex = new RegExp('\\bko\\.components\\.register\\(\s*[\'"]' + this.filename + '[\'"]');
+            if (existingRegistrationRegex.exec(existingContents)) {
+                this.log(chalk.white(this.filename) + chalk.cyan(' is already registered in ') + chalk.white(startupFile));
+                return;
+            }
+            var token = '// [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]';
+            if (this.usesCoffeeScript) {
+                token = '# [Scaffolded component registrations will be inserted here. To retain this feature, don\'t remove this comment.]'
+            };
+            var regex = new RegExp('^(\\s*)(' + token.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&') + ')', 'm'),
+                modulePath = 'components/' + this.filename + '/' + this.filename,
+                lineToAdd = this.usesCoffeeScript ? 'ko.components.register \"' + this.filename + '\", \n\t\trequire: \'' + modulePath + '\'' : 'ko.components.register(\'' + this.filename + '\', { require: \'' + modulePath + '\' });',
+                newContents = existingContents.replace(regex, '$1' + lineToAdd + '\n$&');
+            fs.writeFile(startupFile, newContents);
+            this.log(chalk.green('   registered ') + chalk.white(this.filename) + chalk.green(' in ') + chalk.white(startupFile));
+
+            if (fs.existsSync('gulpfile.js')) {
+                this.log(chalk.magenta('To include in build output, reference ') + chalk.white('\'' + modulePath + '\'') + chalk.magenta(' in ') + chalk.white('gulpfile.js'));
+            }
+        });
+    }
 
 });
 
---
+++ component/templates/viewmodel.coffee
@@ -0,0 +1,16 @@
+define [
+	"knockout"
+	"text!./<%= filename %>.html"
+], (ko, templateMarkup) ->
+
+	class <%= viewModelClassName %>
+		constructor: (params) ->
+			@message = ko.observable("Hello from the <%= name %> component!")
+			# This runs when the component is torn down. Put here any logic necessary to clean up,
+			# for example cancelling setTimeouts or disposing Knockout subscriptions/computeds.
+			@dispose = ->
+
+	return {
+		viewModel: <%= viewModelClassName %> ,
+		template: templateMarkup
+	}
